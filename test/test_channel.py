3b39723083743b18d8273fa7b02db8ede832081e
unknown <bhilarerohit007@gmail.com>
2026-08-31 11:00:03 +0530

    add credential

--- test/test_channel.py
+++ test/test_channel.py
@@ -34,8 +34,8 @@ client = TestClient(app)
 
 def auth_headers():
     return {
-        "Authorization": "Bearer my_super_secret_token_333",
-        "client_id": "my_client_id_456",
+        "Authorization": "Bearer test-token",
+        "X-Client-Id": "test-client",
     }
 
 
